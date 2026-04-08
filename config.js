01ee417a4a25fd95cbe22620f23895256d73768f
unibeatzproduction <syncere862@gmail.com>
2026-04-07 23:15:02 -0400

    Add Firebase and Stripe configuration in config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTStQ25aX1e-sgzOtmcKZPmdJM0NkEaH4",
+  authDomain: "unibeatzproduction-7ae31.firebaseapp.com",
+  projectId: "unibeatzproduction-7ae31",
+  storageBucket: "unibeatzproduction-7ae31.firebasestorage.app",
+  messagingSenderId: "70667820609",
+  appId: "1:70667820609:web:587ca0473d98701600b0c0"
+};
+
+// Stripe configuration
+const stripeConfig = {
+  publishableKey: "pk_live_51TJlJd5uaWK3neNWcnDD3eMIPavl5isfXjl7dp3A3B0f45btaaLPYGxGw1BsXQLik4t9UYqG3JqsJDxJm9IrEE8U00L5ZxnwdU"
+};
