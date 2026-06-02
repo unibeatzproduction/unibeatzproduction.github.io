a78fedb77267c59533ec533a73ac7c3b345f8488
unibeatzproduction <syncere862@gmail.com>
2026-06-02 16:22:13 -0400

    Remove horizontal flip from video styling

--- unifreestyle-cypher.js
+++ unifreestyle-cypher.js
@@ -292,7 +292,7 @@
   if(existing) existing.remove();
   var vid = document.createElement('video');
   vid.autoplay = true; vid.muted = true; vid.playsInline = true;
-  vid.style.cssText = 'position:absolute;top:-10%;left:-10%;width:120%;height:120%;object-fit:cover;transform:scaleX(-1);';
+  vid.style.cssText = 'position:absolute;top:-10%;left:-10%;width:120%;height:120%;object-fit:cover;';
   tile.appendChild(vid);
   track.attach(vid);
 }
